30bd5d07b4cc5a28028cc9f4443141d00cf7cad2
Jeremy Berman <jerber@sas.upenn.edu>
2020-10-05 12:53:58 -0400

    sql error

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from fastapi import FastAPI
 import pprint
 from uszipcode import SearchEngine
 
-search = SearchEngine(simple_zipcode=True)
-
 
 app = FastAPI()
 
@@ -15,6 +13,7 @@ def read_root():
 
 @app.get("/zipcode")
 def get_zipcode(zipcode: str):
+    search = SearchEngine(simple_zipcode=True)
     zipcode = search.by_zipcode(zipcode)
     pprint.pprint(zipcode.to_dict())
     return zipcode.to_dict()
